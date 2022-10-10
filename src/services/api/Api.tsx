461f976fb9c39dee4caf5b8694b54d323e1f88bc
simone-alcala <simone.alcala@gmail.com>
2022-10-10 02:41:16 -0300

    Fix: deploy

--- src/services/api/Api.tsx
+++ src/services/api/Api.tsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const DEV = 'http://localhost:5000';
+const PROD = 'https://simple-expense.herokuapp.com';
 
 export const axiosConfig = axios.create({
-  baseURL: DEV,
+  baseURL: PROD,
   timeout: 3000,  
 });
 
